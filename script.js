662035ca230c23ad529e9073b10635c40e94dbc0
Nur <abdullaevnurdoolot3@gmail.com>
2023-04-02 23:05:27 +0700

    2 задание добавлено

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use script"
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
 
 const personalMovieDB = {
     conunt: numberOfFilms,
@@ -9,8 +9,36 @@ const personalMovieDB = {
     privat: false
 };
 
-const filmName = prompt('Один из последних просмотренных фильмов?'),
-      filmeGrade = +prompt('На сколько оцените его?');
+while(true){
+    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    if (numberOfFilms<100){
+        console.log('просмотрено довольно мало фильмов')
+    }else if(10 <= numberOfFilms >= 30){
+        console.log('Вы классический зритель')
+    }else if(numberOfFilms>30){
+        console.log('Вы киноман')
+    }else{
+        console.log('Произошла ошибка')
+    }
+    const filmName = prompt('Один из последних просмотренных фильмов?', '');
+    if(filName === ''){
+        console.log('Строка не может быть пустой!');
+        continue;
+    }else if(filmName.length===50){
+        console.log('Количество символов не должно превышать 50.');
+        continue;
+    }
+    const filmeGrade = +prompt('На сколько оцените его?', '');
+    if(filmeGrade === ''){
+        console.log('Строка не может быть пустой!');
+        continue;
+    }else if(filmeGrade.length===50){
+        console.log('Количество символов не должно превышать 50.');
+        continue;
+    }
+    personalMovieDB.movies[filmName]=filmeGrade;
+}
 
-personalMovieDB.movies[filmName]=filmeGrade;
+
+
 console.log(personalMovieDB.movies);
