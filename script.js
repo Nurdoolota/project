c181f439178422217f67e4a811f43ab838d76b65
Nur <abdullaevnurdoolot3@gmail.com>
2023-04-03 11:13:57 +0700

    исправление 2 задания

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 "use script"
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-if (numberOfFilms<10){
-    console.log('просмотрено довольно мало фильмов')
-}else if(10 <= numberOfFilms >= 30){
-    console.log('Вы классический зритель')
-}else if(numberOfFilms>30){
-    console.log('Вы киноман')
-}else{
-    console.log('Произошла ошибка')
-}
+// if (numberOfFilms<10){
+//     console.log('просмотрено довольно мало фильмов')
+// }else if(10 <= numberOfFilms >= 30){
+//     console.log('Вы классический зритель')
+// }else if(numberOfFilms>30){
+//     console.log('Вы киноман')
+// }else{
+//     console.log('Произошла ошибка')
+// }
 
 const personalMovieDB = {
     conunt: numberOfFilms,
@@ -19,42 +19,19 @@ const personalMovieDB = {
     privat: false
 };
 
-// while(true){
+
+
+for (let i=0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
     
-//     const filmName = prompt('Один из последних просмотренных фильмов?', '');
-//     if(filmName === ''){
-//         console.log('Строка не может быть пустой!');
-//         continue;
-//     }else if(filmName.length==='50'){
-//         console.log('Количество символов не должно превышать 50.');
-//         continue;
-//     }
-//     const filmeGrade = +prompt('На сколько оцените его?', '');
-//     if(filmeGrade === ''){
-//         console.log('Строка не может быть пустой!');
-//         continue;
-//     }else if(filmeGrade.length==='50'){
-//         console.log('Количество символов не должно превышать 50.');
-//         continue;
-//     }
-//     personalMovieDB.movies[filmName]=filmeGrade;
-// }
-
-for (let i = 0; i < 2; i++){
-    const filmName = prompt('Один из последних просмотренных фильмов?', ''),
-        filmeGrade = +prompt('На сколько оцените его?', '');
-
-    if(filmName!=null && filmeGrade != null && filmName != '' &&        filmeGrade != '' && filmName.lenght < 50){
+    if(a != null && b != null && a != '' && b != '' && a.lenght < 50){
+        personalMovieDB.movies[a]=b;
         console.log('done');
-    }else{
+    }else {
         console.log('error');
         i--;
-
     }
-
-    personalMovieDB.movies[filmName]=filmeGrade;
 }
-
-
 
 console.log(personalMovieDB.movies);
