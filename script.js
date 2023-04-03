8907658c626169f8f9850212314ac315a0ff739d
Nur <abdullaevnurdoolot3@gmail.com>
2023-04-03 10:56:16 +0700

    внесены изменения по 2 заданию

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 "use script"
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (numberOfFilms<10){
+    console.log('просмотрено довольно мало фильмов')
+}else if(10 <= numberOfFilms >= 30){
+    console.log('Вы классический зритель')
+}else if(numberOfFilms>30){
+    console.log('Вы киноман')
+}else{
+    console.log('Произошла ошибка')
+}
 
 const personalMovieDB = {
     conunt: numberOfFilms,
@@ -9,33 +19,39 @@ const personalMovieDB = {
     privat: false
 };
 
-while(true){
-    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    if (numberOfFilms<100){
-        console.log('просмотрено довольно мало фильмов')
-    }else if(10 <= numberOfFilms >= 30){
-        console.log('Вы классический зритель')
-    }else if(numberOfFilms>30){
-        console.log('Вы киноман')
+// while(true){
+    
+//     const filmName = prompt('Один из последних просмотренных фильмов?', '');
+//     if(filmName === ''){
+//         console.log('Строка не может быть пустой!');
+//         continue;
+//     }else if(filmName.length==='50'){
+//         console.log('Количество символов не должно превышать 50.');
+//         continue;
+//     }
+//     const filmeGrade = +prompt('На сколько оцените его?', '');
+//     if(filmeGrade === ''){
+//         console.log('Строка не может быть пустой!');
+//         continue;
+//     }else if(filmeGrade.length==='50'){
+//         console.log('Количество символов не должно превышать 50.');
+//         continue;
+//     }
+//     personalMovieDB.movies[filmName]=filmeGrade;
+// }
+
+for (let i = 0; i < 2; i++){
+    const filmName = prompt('Один из последних просмотренных фильмов?', ''),
+        filmeGrade = +prompt('На сколько оцените его?', '');
+
+    if(filmName!=null && filmeGrade != null && filmName != '' &&        filmeGrade != '' && filmName.lenght < 50){
+        console.log('done');
     }else{
-        console.log('Произошла ошибка')
+        console.log('error');
+        i--;
+
     }
-    const filmName = prompt('Один из последних просмотренных фильмов?', '');
-    if(filName === ''){
-        console.log('Строка не может быть пустой!');
-        continue;
-    }else if(filmName.length===50){
-        console.log('Количество символов не должно превышать 50.');
-        continue;
-    }
-    const filmeGrade = +prompt('На сколько оцените его?', '');
-    if(filmeGrade === ''){
-        console.log('Строка не может быть пустой!');
-        continue;
-    }else if(filmeGrade.length===50){
-        console.log('Количество символов не должно превышать 50.');
-        continue;
-    }
+
     personalMovieDB.movies[filmName]=filmeGrade;
 }
 
